86df0efb389cfb1d255ae2c69f1949204b0c570d
BardiaR <bardia.d1375@gmail.com>
2023-05-04 21:33:53 +0330

    fixed problem in movement tiles

--- src/Components/Boards/index.js
+++ src/Components/Boards/index.js
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 
 import "./style.scss";
 import Overlay from "../Overlay";
@@ -15,35 +15,29 @@ const Boards = () => {
   const [numbers, setNumbers] = useState(tiles());
 
   const moveTiles = (tile) => {
-    const index16 = numbers.find((item) => item.value === 16);
+    const index16 = numbers.find((item) => item.value === 16).index;
     if (
       ![index16 - 1, index16 + 1, index16 - 4, index16 + 4].includes(tile.index)
-    ) {
+    )
       return;
-    }
-    const newNumbers = [...numbers].map((number, index) => {
-      if (number !== index16 && number.index !== tile.index) {
-        return number;
-      }
-      else if (number.value === 16) {
-        return {value: 16, index : tile.index}
-      }
-      else {
-        return {value: tile.value, index: 16}
-      }
+
+    let newNumbers = [...numbers].map((number) => {
+      if (number.index !== index16 && number.index !== tile.index) return number;
+      else if (number.value === 16) return { value: 16, index: tile.index };
+
+      return { value: tile.value, index: index16 };
     });
 
-    setNumbers(newNumbers)
+    setNumbers(newNumbers);
   };
-
 
   return (
     <div className="board-wrapper">
       <div className="board">
-          <Overlay />
-          {
-            numbers.map((number,index) => (<Tiles key={index} number={number} moveTiles={moveTiles} />))
-          }
+        <Overlay />
+        {numbers.map((number, index) => (
+          <Tiles key={index} number={number} moveTiles={moveTiles} />
+        ))}
       </div>
     </div>
   );
